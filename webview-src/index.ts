1af3c54555522a311f3aee6ab63fd2e9df7a161e
Keegan Stothert <keeganstothert@protonmail.com>
2024-01-11 10:51:19 -0700

    feat: android geolocation tested and working

--- webview-src/index.ts
+++ webview-src/index.ts
@@ -5,5 +5,6 @@ import { invoke } from "@tauri-apps/api/core";
 // }
 
 export async function getLocation() {
+  console.log("[tauri-plugin-geolocation] getLocation");
   return await invoke("plugin:geolocation|getLocation");
 }
